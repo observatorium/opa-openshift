c236f0f5f38347bf2dd4f9e58bea6ff3d1612259
Robert Jacob <rojacob@redhat.com>
2022-09-12 16:42:16 +0200

    Add unit tests for matcher handling

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -134,48 +133,5 @@ func New(l log.Logger, c cache.Cacher, wt transport.WrapperFunc, cfg *config.Con
 
 			return
 		}
-	}
-}
-
-func prepareMap(csvInput string) map[string]struct{} {
-	if csvInput == "" {
-		return nil
-	}
-
-	tokens := strings.Split(csvInput, ",")
-
-	skipMap := make(map[string]struct{}, len(tokens))
-	for _, token := range tokens {
-		if token == "" {
-			continue
-		}
-
-		skipMap[token] = struct{}{}
-	}
-
-	return skipMap
-}
-
-func createMatcherFunc(cfg config.OPAConfig) func(tenant string, groups []string) string {
-	matcher := cfg.Matcher
-	skipTenants := prepareMap(cfg.MatcherSkipTenants)
-	adminGroups := prepareMap(cfg.MatcherAdminGroups)
-
-	return func(tenant string, groups []string) string {
-		if matcher == "" {
-			return ""
-		}
-
-		if _, skip := skipTenants[tenant]; skip {
-			return ""
-		}
-
-		for _, group := range groups {
-			if _, admin := adminGroups[group]; admin {
-				return ""
-			}
-		}
-
-		return matcher
 	}
 }
---
+++ internal/handler/matcher_test.go
@@ -0,0 +1,82 @@
+package handler
+
+import (
+	"github.com/observatorium/opa-openshift/internal/config"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMatcherForRequest(t *testing.T) {
+	tt := []struct {
+		desc        string
+		opaConfig   config.OPAConfig
+		tenant      string
+		groups      []string
+		wantMatcher string
+	}{
+		{
+			desc:        "empty matcher",
+			opaConfig:   config.OPAConfig{},
+			tenant:      "",
+			groups:      []string{},
+			wantMatcher: "",
+		},
+		{
+			desc: "normal",
+			opaConfig: config.OPAConfig{
+				Matcher:            "test-matcher",
+				MatcherSkipTenants: "tenantB,tenantC",
+				MatcherAdminGroups: "admin-group,other-admin-group",
+			},
+			tenant:      "tenantA",
+			groups:      []string{"authenticated"},
+			wantMatcher: "test-matcher",
+		},
+		{
+			desc: "skip empty group",
+			opaConfig: config.OPAConfig{
+				Matcher:            "test-matcher",
+				MatcherSkipTenants: "tenantB,tenantC",
+				MatcherAdminGroups: "admin-group,,other-admin-group",
+			},
+			tenant:      "tenantA",
+			groups:      []string{""},
+			wantMatcher: "test-matcher",
+		},
+		{
+			desc: "tenant skipped",
+			opaConfig: config.OPAConfig{
+				Matcher:            "test-matcher",
+				MatcherSkipTenants: "tenantB,tenantC",
+				MatcherAdminGroups: "admin-group,other-admin-group",
+			},
+			tenant:      "tenantB",
+			groups:      []string{"authenticated"},
+			wantMatcher: "",
+		},
+		{
+			desc: "user is admin",
+			opaConfig: config.OPAConfig{
+				Matcher:            "test-matcher",
+				MatcherSkipTenants: "tenantB,tenantC",
+				MatcherAdminGroups: "admin-group,other-admin-group",
+			},
+			tenant:      "tenantA",
+			groups:      []string{"admin-group"},
+			wantMatcher: "",
+		},
+	}
+
+	for _, tc := range tt {
+		tc := tc
+		t.Run(tc.desc, func(t *testing.T) {
+			t.Parallel()
+
+			matcherForRequest := createMatcherFunc(tc.opaConfig)
+
+			matcher := matcherForRequest(tc.tenant, tc.groups)
+
+			require.Equal(t, tc.wantMatcher, matcher)
+		})
+	}
+}
